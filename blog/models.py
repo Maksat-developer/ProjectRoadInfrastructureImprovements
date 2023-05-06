3057951893683465758b39fc89ee44b4486dcfe7
kubanuch03 <kubanuch03@gmail.com>
2023-05-06 19:42:23 +0600

    change blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -12,13 +12,12 @@ LEVEL_CHOICES = (
 
 class MessageModel(models.Model):
 
-    description = models.TextField(max_length=250,verbose_name='Описание')
+    description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to ='media/%Y/%m/%d',verbose_name='Изображение')
-    category = models.ForeignKey('Category',max_length=100,on_delete = models.CASCADE ,verbose_name='Категория')
+    category = models.ForeignKey('Category',on_delete = models.CASCADE ,verbose_name='Категория')
     created_at = models.DateField(auto_now=True,verbose_name='Дата заявки')
     is_renovated = models.BooleanField(default=False,verbose_name='Отремонтирован')   #Отремонтирована или нет
     choices = models.CharField(
-        max_length=5,
         choices = LEVEL_CHOICES,
          default = '1'        
         )
