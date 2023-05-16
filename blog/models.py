37a4c358a70499645340864cead0df254ff2ae81
kubanuch03 <kubanuch03@gmail.com>
2023-05-16 16:37:04 +0600

    create blog/serializer, add blog/urls.py,add blog/views.py add models.py

--- blog/models.py
+++ blog/models.py
@@ -1,25 +1,29 @@
 from django.db import models
 
 
-LEVEL_CHOICES = (
-    ('1','нормально')
-    ('2','не опасный')
-    ('3','умеренный')
-    ('4','опасный')
-    ('5','очень опасный')
-)
+LEVEL_CHOICES = [
+    ('1','1'),#нормально
+    ('2','2'),#не опасный
+    ('3','3'),#умеренный
+    ('4','4'),#опасный
+    ('5','5'),#очень опасный
+]
 
 
 class MessageModel(models.Model):
 
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to ='media/%Y/%m/%d',verbose_name='Изображение')
-    category = models.ForeignKey('Category',on_delete = models.CASCADE ,verbose_name='Категория')
+    # category = models.ForeignKey('Category',on_delete = models.CASCADE ,verbose_name='Категория')
     created_at = models.DateField(auto_now=True,verbose_name='Дата заявки')
     is_renovated = models.BooleanField(default=False,verbose_name='Отремонтирован')   #Отремонтирована или нет
     choices = models.CharField(
+        max_length=1,
         choices = LEVEL_CHOICES,
-         default = '1'        
+        default = '1'        
         )
     quantity = models.IntegerField(verbose_name='')#    ???????
+
+    def __str__(self) -> str:
+        return self.description
     
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .serializers import MessageModelSerializer
+from rest_framework import generics
+from .models import MessageModel
 
-# Create your views here.
+
+
+class MessageModelListCreate(generics.ListCreateAPIView):
+    queryset = MessageModel.objects.all()
+    serializer_class = MessageModelSerializer
+
+
+
+class MessageModelDelete(generics.RetrieveDestroyAPIView):
+    queryset = MessageModel.objects.all()
+    serializer_class = MessageModelSerializer
+
+
+
+class MessageModelUpdate(generics.RetrieveUpdateAPIView):
+    queryset = MessageModel.objects.all()
+    serializer_class = MessageModelSerializer
+
+
+
